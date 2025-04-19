da7918a8b96c3f0015991256a144b30b6a5b2dcf
artur <arturcamargo242@gmail.com>
2025-04-19 10:34:15 -0300

    Implement jump instruction handling in the ALU

--- launch.c
+++ launch.c
@@ -27,6 +27,21 @@ void alu(int opcode, int *registers[4], int op1, int op2, int op3) {
   }
 }
 
+int handle_jump(int opcode, int *regs[], Instruction instr, int current_pc) {
+  switch (opcode) {
+      case JMP:
+          return instr.op1;
+      case JEQ:
+          return (*regs[instr.op1] == *regs[instr.op2]) ? instr.op3 : current_pc;
+      case JGT:
+          return (*regs[instr.op1] > *regs[instr.op2]) ? instr.op3 : current_pc;
+      case JLT:
+          return (*regs[instr.op1] < *regs[instr.op2]) ? instr.op3 : current_pc;
+      default:
+          return current_pc;
+  }
+}
+
 void launch_program(int data_memory[MEMORY_SIZE], Instruction memory[MEMORY_SIZE]) {
   int pc = 0, a0 = 0, a1 = 0, a2 = 0, a3 = 0;
   int *registers[4] = { &a0, &a1, &a2, &a3 };
@@ -49,21 +64,12 @@ void launch_program(int data_memory[MEMORY_SIZE], Instruction memory[MEMORY_SIZE
         case ST:
             data_memory[instruction.op1] = *registers[instruction.op2];
             break;
-
+        
         case JMP:
-            pc = instruction.op1;
-            break;
-
         case JEQ:
-            if (*registers[instruction.op1] == *registers[instruction.op2]) pc = instruction.op3;
-            break;
-
         case JGT:
-            if (*registers[instruction.op1] > *registers[instruction.op2]) pc = instruction.op3;
-            break;
-
         case JLT:
-            if (*registers[instruction.op1] < *registers[instruction.op2]) pc = instruction.op3;
+            pc = handle_jump(instruction.opcode, registers, instruction, pc);
             break;
 
         case W:
